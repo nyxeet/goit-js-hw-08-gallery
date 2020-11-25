90bc5066f9e2a4b07b8f21c2f0cc8e553bc1e34c
nyxeet <nyxeet@ukr.net>
2020-11-26 01:11:47 +0200

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,8 +1,43 @@
 import gallery from './gallery-items.js'
 
-
+const modalRefs = {
+    closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
+    modal: document.querySelector('.lightbox'),
+    overlay: document.querySelector('.lightbox__overlay'),
+    image: document.querySelector('.lightbox__image'),
+};
 
 const galleryRef = document.querySelector('.js-gallery')
+
+
+galleryRef.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target.nodeName != 'IMG') {
+        return;
+    }
+    modalRefs.image.setAttribute('src', event.target.dataset.source) 
+    modalRefs.modal.classList.add('is-open')
+})
+
+window.addEventListener('keydown', event => {
+    if (modalRefs.modal.classList.contains('is-open')) {
+        if (event.code === 'Escape') {
+          modalRefs.modal.classList.remove('is-open');
+          modalRefs.image.setAttribute('src', '');
+        }
+    }
+})
+
+modalRefs.closeModalBtn.addEventListener('click', event => {
+    modalRefs.modal.classList.remove('is-open');
+    modalRefs.image.setAttribute('src', '');
+})
+
+modalRefs.overlay.addEventListener('click', event => {
+    modalRefs.modal.classList.remove('is-open');
+    modalRefs.image.setAttribute('src', '');
+})
+
 
 
 const createGalleryImage = element => {
@@ -18,7 +53,6 @@ const createGalleryImage = element => {
     imageRef.setAttribute('alt', element.description)
     linkRef.append(imageRef);
     liRef.append(linkRef);
-    console.log(imageRef.dataset.source)
     return liRef;
 }
 const galleryListRef = gallery.map(element => createGalleryImage(element))
