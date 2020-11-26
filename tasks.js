22eeeb6d05ee51a5ecd080a624078c46eca09d3c
nyxeet <nyxeet@ukr.net>
2020-11-26 02:54:03 +0200

    главное что работает))))))))

--- tasks.js
+++ tasks.js
@@ -1,45 +1,19 @@
 import gallery from './gallery-items.js'
-
+const galleryRef = document.querySelector('.js-gallery')
 const modalRefs = {
     closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
+    closeModalOverlay: document.querySelector('.lightbox__overlay'),
     modal: document.querySelector('.lightbox'),
-    overlay: document.querySelector('.lightbox__overlay'),
     image: document.querySelector('.lightbox__image'),
 };
 
-const galleryRef = document.querySelector('.js-gallery')
 
+galleryRef.addEventListener('click', openModal);
+window.addEventListener('keydown', closeModalByEscape)
+modalRefs.closeModalBtn.addEventListener('click', closeModalByClick)
+modalRefs.closeModalOverlay.addEventListener('click', closeModalByClick)
 
-galleryRef.addEventListener('click', event => {
-    event.preventDefault();
-    if (event.target.nodeName != 'IMG') {
-        return;
-    }
-    modalRefs.image.setAttribute('src', event.target.dataset.source) 
-    modalRefs.modal.classList.add('is-open')
-})
-
-window.addEventListener('keydown', event => {
-    if (modalRefs.modal.classList.contains('is-open')) {
-        if (event.code === 'Escape') {
-          modalRefs.modal.classList.remove('is-open');
-          modalRefs.image.setAttribute('src', '');
-        }
-    }
-})
-
-modalRefs.closeModalBtn.addEventListener('click', event => {
-    modalRefs.modal.classList.remove('is-open');
-    modalRefs.image.setAttribute('src', '');
-})
-
-modalRefs.overlay.addEventListener('click', event => {
-    modalRefs.modal.classList.remove('is-open');
-    modalRefs.image.setAttribute('src', '');
-})
-
-
-
+let dataIndex = 1; 
 const createGalleryImage = element => {
     const liRef = document.createElement('li')
     liRef.classList.add('gallery__item');
@@ -53,7 +27,82 @@ const createGalleryImage = element => {
     imageRef.setAttribute('alt', element.description)
     linkRef.append(imageRef);
     liRef.append(linkRef);
+    imageRef.setAttribute('data-index', dataIndex);
+    dataIndex += 1;
     return liRef;
 }
 const galleryListRef = gallery.map(element => createGalleryImage(element))
 galleryRef.append(...galleryListRef)
+dataIndex = 1;
+
+
+const imagesRef = document.querySelectorAll('.gallery__image')
+console.log(imagesRef)
+
+
+
+
+window.addEventListener('keydown', event => {
+    console.log(event.key)
+})
+
+window.addEventListener('keydown', event => {
+    if (modalRefs.modal.classList.contains('is-open')) {
+        if (event.key == 'ArrowLeft') {
+            const currentIndex = modalRefs.image.dataset.index;
+            const newElement = getLeftElement(imagesRef, currentIndex);
+            console.log(newElement)
+            modalRefs.image.setAttribute('src', newElement);
+            modalRefs.image.setAttribute('data-index', Number(currentIndex) - 1);
+        }
+    }
+})
+window.addEventListener('keydown', event => {
+    if (modalRefs.modal.classList.contains('is-open')) {
+        if (event.key == 'ArrowRight') {
+            const currentIndex = modalRefs.image.dataset.index;
+            const newElement = getRightElement(imagesRef, currentIndex);
+            modalRefs.image.setAttribute('src', newElement);
+            modalRefs.image.setAttribute('data-index', Number(currentIndex) + 1);
+        }
+    }
+})
+function getRightElement(elements, index) {
+    for (let i = 0; i <= elements.length - 1; i += 1){
+        if (elements[i].dataset.index == Number(index) + 1) {
+            return elements[i].dataset.source;
+        }
+    }
+}
+function getLeftElement(elements, index) {
+    for (let i = 0; i <= elements.length - 1; i += 1){
+        if (elements[i].dataset.index == Number(index) - 1) {
+            return elements[i].dataset.source;
+        }
+    }
+}
+
+
+function closeModalByClick(event) {
+    modalRefs.modal.classList.remove('is-open');
+    modalRefs.image.setAttribute('src', '');
+}
+function closeModalByEscape(event) {
+    if (modalRefs.modal.classList.contains('is-open')) {
+        if (event.code === 'Escape') {
+          modalRefs.modal.classList.remove('is-open');
+          modalRefs.image.setAttribute('src', '');
+        }
+    }
+}
+function openModal(event){
+    event.preventDefault();
+    if (event.target.nodeName != 'IMG') {
+        return;
+    }
+    modalRefs.image.setAttribute('src', event.target.dataset.source) 
+    modalRefs.image.setAttribute('data-index', event.target.dataset.index)
+    modalRefs.modal.classList.add('is-open')
+}
+
+
